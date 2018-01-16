ecb5bd5ab177daa9341ffc7497371a87e1092a85
Steven DeLoach <sfdeloach@gmail.com>
2018-01-15 22:35:23 -0500

    navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  showSquads: boolean = false;
+  timerID: any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleSquads() {
+    this.showSquads = !this.showSquads;
+  }
+
+  onExit() {
+    // start timer
+    this.timerID = setTimeout(() => {
+      this.showSquads = false;
+    }, 2500);
+  }
+
+  onEnter() {
+    // stop timer
+    clearTimeout(this.timerID);
   }
 
 }
